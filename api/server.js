b7f1f76cb69c04fba0ff6f0f1bf8093041e9a809
İrem Çelebi <iremcelebi2404@gmail.com>
2023-04-12 04:35:30 +0300

    tweets end pointleri tamamlandı

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const authRouter = require("./auth/auth-router.js");
 const usersRouter = require("./users/users-router.js");
+const tweetsRouter = require("./tweets/tweets-router.js");
 const authMd = require("./auth/auth-middleware.js");
+
 const server = express();
 server.use(express.json());
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", authMd.tokenKontrolu, usersRouter);
+server.use("/api/tweets", authMd.tokenKontrolu, tweetsRouter);
 
 server.use((req, res, next) => {
   res.status(404).send("Aradığınız adres bulunamadı");
